1f581cb363410faa54b09a6d2bc3af543dc56c54
Thanh Luan <luannguyengl2001@gmail.com>
2025-03-26 13:14:06 +0700

    fix animation

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 function openNav() {
   document.getElementById("mobile-nav").style.display = "flex";
 }
@@ -7,10 +6,5 @@ function closeNav() {
   document.getElementById("mobile-nav").style.display = "none";
 }
 setTimeout(() => {
-  document.getElementById('road1').style.display = "none";
-  document.getElementById('road2').style.display = "flex";
-  document.getElementById('road2').style.opacity = "1";
-  document.getElementById('road2').style.zIndex = "2";
-  document.getElementById('road2').style.visibility = "unset";
   document.getElementById('span').innerHTML = "15%"
-}, 1500);
+}, 1000);
