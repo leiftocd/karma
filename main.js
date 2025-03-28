2a0fd0a90f930574dd0604fca3e1e832ed6c5211
Thanh Luan <luannguyengl2001@gmail.com>
2025-03-28 13:21:36 +0700

    delete space

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-function openNav() {
-  document.getElementById("mobile-nav").style.display = "flex";
-}
-
-function closeNav() {
-  document.getElementById("mobile-nav").style.display = "none";
-}
-
 setTimeout(() => {
   document.getElementById('span').innerHTML = "15%"
 }, 1000);
