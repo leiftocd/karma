4d3117beeb37d254f014b7001f950ecf7cc55b37
Thanh Luan <luannguyengl2001@gmail.com>
2025-03-28 11:36:57 +0700

    change logo +insert content

--- main.js
+++ main.js
@@ -5,6 +5,23 @@ function openNav() {
 function closeNav() {
   document.getElementById("mobile-nav").style.display = "none";
 }
+
 setTimeout(() => {
   document.getElementById('span').innerHTML = "15%"
 }, 1000);
+
+document.getElementById("lang-switch").addEventListener("change", function () {
+  let lang = this.value;
+  let cnContent = document.getElementById("karma-content_cn");
+  let enContent = document.getElementById("karma-content_en");
+
+  if (lang === "en") {
+      enContent.classList.remove("hidden");
+      cnContent.classList.add("hidden");
+  } else {
+      enContent.classList.add("hidden");
+      cnContent.classList.remove("hidden");
+  }
+});
+
+document.getElementById("lang-switch").dispatchEvent(new Event("change"));
